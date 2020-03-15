e531e9be6b0e82d098631d82f0f6503dc09091b4
Olav Kallhovd <olav.kallhovd@gmail.com>
2020-03-15 13:04:31 +0100

    Update mqtt_shutdown.py

--- mqtt_shutdown.py
+++ mqtt_shutdown.py
@@ -1,17 +1,21 @@
-
 #!/usr/bin/env python
 import paho.mqtt.client as mqtt
 import time
 from subprocess import call
 
-# Use MQTT for remote reboot and shutdown
-# copy this file to /usr/local/bin (if other path is used, then also change the service script file)
+#configuration:
+brokerAdr = "192.168.1.199"
+brokerPort = 1883 
+brokerUserName = "myMqttUser"
+brokerPassword = "myMqttpassword"
+teleTopic = "tele/myRPI/ncs"
+cmndTopic = "cmnd/myRpi"
 
 # "on connect" event
 def connectFunction (client, userdata, flags, rc):
   if rc==0:
     print("connected OK Returned code=",rc)
-    MyClient.publish("stat/RPI_test", "online") # Publish message to MQTT broker
+    MyClient.publish(teleTopic, "Online") # Publish message to MQTT broker
   else:
     print("Bad connection Returned code=",rc)
 
@@ -20,7 +24,7 @@ def messageFunction (client, userdata, message):
   topic = str(message.topic)
   payload = str(message.payload.decode("utf-8"))
   print("New message received:", topic+" "+payload)
-  if topic=="cmnd/RPI_test":
+  if topic==cmndTopic:
     handleCmnd(payload)
 
 # handle new MQTT command function
@@ -33,15 +37,15 @@ def handleCmnd (cmnd):
     print("'test' received")
 
 MyClient = mqtt.Client() # Create a MQTT client object
-#MyClient.username_pw_set("my_mqtt_user", "my_mqtt_password")
+MyClient.username_pw_set(brokerUserName, brokerPassword)
 MyClient.on_connect = connectFunction # run function on connect with broker
-MyClient.will_set("stat/RPI_test", "Offline", 0, True)
-MyClient.connect("192.168.1.4", 1883) # Connect to the test MQTT broker
-MyClient.subscribe("cmnd/RPI_test") # Subscribe to a topic
+MyClient.will_set(teleTopic, "Offline", 0, True)
+MyClient.connect(brokerAdr, brokerPort) # Connect to the test MQTT broker
+MyClient.subscribe(cmndTopic) # Subscribe to a topic
 MyClient.on_message = messageFunction # Attach the messageFunction to subscription
 MyClient.loop_start() # Start the MQTT client
 
 
 while(1):
-  MyClient.publish("stat/RPI_test", "online") # Publish message to MQTT broker
+  MyClient.publish(teleTopic, "Online") # Publish message to MQTT broker
   time.sleep(60) # Sleep for a while
